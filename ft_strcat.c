db61b972d5a2f8b1c8ddcf0d6c7a3bae3874a724
chan-sen <chansen1532@hotmail.com>
2016-09-28 12:09:03 -0700

    working

--- ft_strcat.c
+++ ft_strcat.c
@@ -3,49 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpassafa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: chansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/22 17:10:45 by rpassafa          #+#    #+#             */
-/*   Updated: 2016/09/22 17:10:47 by rpassafa         ###   ########.fr       */
+/*   Created: 2016/09/25 02:19:43 by chansen           #+#    #+#             */
+/*   Updated: 2016/09/25 02:19:44 by chansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_lib.h"
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
-char *ft_strcat(char *dest, const char *src)
+char	*ft_strcat(char *s1, const char *s2)
 {
-	int start;
-	int i;
-	
-	char *temp;
-	temp = malloc(sizeof(dest));
-	ft_strcpy(temp,dest);
-	dest = malloc(ft_strlen(dest) + ft_strlen(src));
-	ft_strcpy(dest,temp);
-	i = 0;
-	start = ft_strlen(dest);
-	while(src[i])
-	{
-		dest[start] = src[i];
-		start++;
-		i++;
-	}
-	dest[start] = '\0';
-	return(dest);
-}
+	char	*str = s1;
 
-int main()
-{
-	printf("%s\n",ft_strcat("hi","bye"));
-	printf("%s\n",ft_strcat(" "," "));
-	printf("%s\n",ft_strcat("yess","noo"));
-
-	printf("%s\n", "----------------");
-
-	printf("%s\n",strcat("heei","bie"));
-	printf("%s\n",strcat("dick","chick"));
-	printf("%s\n",strcat("right","now"));
-	return 0;
+	if (*s1)
+		while (*++s1);
+	while ((*s1++ = *s2++));
+	return (str);
 }
